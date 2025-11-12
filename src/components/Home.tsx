5b3b69497c24bea8fe04edc350476d1984333528
edward <edwardlfish@gmail.com>
2025-11-12 12:37:33 +0200

    Add dynamic hero routing and context integration in Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,22 @@
 import Hero from "./Hero.tsx";
 import DreamTeam from "./DreamTeam.tsx";
 import OpeningCrawl from "./OpeningCrawl.tsx";
+import {SWContext} from "../utils/context.ts";
+import {useContext, useEffect} from "react";
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router";
 
 const Home = () => {
+    const {heroId = defaultHero} = useParams();
+    const {changeHero} = useContext(SWContext);
+
+    useEffect(() => {
+        if (!(heroId in characters)) {
+            return;
+        }
+        changeHero(heroId);
+    }, [])
+
     return (
         <main>
             <Hero/>
