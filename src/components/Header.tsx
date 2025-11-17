dc5d058b8d452c45db02941366d9d0c11803042b
edward <edwardlfish@gmail.com>
2025-11-17 10:37:26 +0200

    Refactor hero handling logic to allow optional hero values and update related components and types

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,12 @@ import {SWContext} from "../utils/context.ts";
 
 const Header = () => {
     const {hero} = useContext(SWContext)
+    const title = hero ? characters[hero].name : 'Error';
 
     return (
         <header className="rounded-t-3xl bg-grey">
             <Navigation/>
-            <h1 className="text-center py-6 text-4xl">{characters[hero].name}</h1>
+            <h1 className="text-center py-6 text-4xl">{title}</h1>
         </header>
     );
 };
